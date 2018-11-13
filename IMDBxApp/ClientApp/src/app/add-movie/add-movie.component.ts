f4eef6afd84e54091b47b85c90fbb152b19907fa
arv@git <aravindanil816@gmail.com>
2018-11-14 00:20:43 +0530

    Red astrisk

--- IMDBxApp/ClientApp/src/app/add-movie/add-movie.component.ts
+++ IMDBxApp/ClientApp/src/app/add-movie/add-movie.component.ts
@@ -70,12 +70,11 @@ export class AddMovieComponent implements OnInit {
     const headers = new HttpHeaders().set('content-type', 'application/json');
     this.http.post(this.baseUrl + 'api/Home/SaveNewActor', this.newActor, { headers }).subscribe(result => {
       this.newActor.actorId = Number(result);
-      /*document.getElementsByClassName('close').forEach((element: HTMLElement, index) => {
-        element.click();
-      });*/
+      (document.getElementById("actor-modal-close") as HTMLButtonElement).click();
       this.actorsList.push(this.newActor);
       this.actorTags.push({ 'display': this.newActor.actorName, 'value': this.newActor });
       console.log(result);
+      (document.getElementById("show-result") as HTMLButtonElement).click();
 
     }, error => console.error(error));
   }  
@@ -86,13 +85,11 @@ export class AddMovieComponent implements OnInit {
     const headers = new HttpHeaders().set('content-type', 'application/json');
     this.http.post(this.baseUrl + 'api/Home/SaveNewProducer', this.newProducer, { headers }).subscribe(result => {
       this.newProducer.prodId = Number(result);
-      /*document.getElementsByClassName('close').forEach((element: HTMLElement, index) => {
-        element.click();
-      })*/
+      (document.getElementById("producer-modal-close") as HTMLButtonElement).click();
       this.producerList.push(this.newProducer);
       this.producerTags.push({ 'display': this.newProducer.prodName, 'value': this.newProducer });
       console.log(result);
-
+  (document.getElementById("show-result") as HTMLButtonElement).click();
     }, error => console.error(error));
   }
 
@@ -103,6 +100,7 @@ export class AddMovieComponent implements OnInit {
     const headers = new HttpHeaders().set('content-type', 'application/json');
     this.http.post(this.baseUrl + 'api/Home/UpdateMovie', this.newmovie_Master, { headers }).subscribe(result => {
       this.newmovie.movieId = Number(result);
+      (document.getElementById("show-result") as HTMLButtonElement).click();
     }, error => console.error(error));
   }
 
